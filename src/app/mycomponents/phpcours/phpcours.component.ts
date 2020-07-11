7b3b0a50ac6e20d909af5a6c23a57e0527e0048f
zhen <zhen@MacBook-Air-de-zhen.local>
2020-07-11 15:49:55 +0200

    maj

--- src/app/mycomponents/phpcours/phpcours.component.ts
+++ src/app/mycomponents/phpcours/phpcours.component.ts
@@ -9,6 +9,8 @@ import { StoreService } from '../../myservices/store.service';
 })
 export class PhpcoursComponent implements OnInit {
 
+  bigimg:boolean = false;
+  bigimgurl:string = null;
   lesson:string;
   fileurl:string;
   mydisplay = [true,true,true,true,true];
@@ -17,15 +19,26 @@ export class PhpcoursComponent implements OnInit {
     this.fileurl = this.ss.getFileUrl();
    }
 
-  ngOnInit() {
+  ngOnInit() :void{
     this.s1.lesson$.subscribe((data)=>{
       this.lesson = data[0];
     });
   }
 
-  showContent(index){
+  showContent(index:number):void{
     if(this.mydisplay[index] == true)this.mydisplay[index] = false;
     else this.mydisplay[index] = true;
+  }
+
+  swithBigimg(e:any,n:number):void{
+    if(n == 0){
+      this.bigimg = false;
+      this.bigimgurl = null;
+    }
+    if(n == 1){
+      this.bigimgurl = e.target.src;
+      this.bigimg = true;
+    }
   }
 
 }
