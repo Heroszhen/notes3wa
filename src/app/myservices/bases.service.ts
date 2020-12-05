7ce33a29d829624f2f458ecd7cbc37192b8a8bd6
Heroszhen <zhen.yang@hotmail.fr>
2020-12-05 17:48:11 +0100

    maj

--- src/app/myservices/bases.service.ts
+++ src/app/myservices/bases.service.ts
@@ -9,7 +9,7 @@ export class BasesService {
   protected server:string;
   constructor() {
     //this.filesUrl = "http://localhost:8000/";
-    this.filesUrl = "http://notes3wa.yangzhen.fr/notes3WAfiles/";
+    this.filesUrl = "https://notes3wa.yangzhen.fr/notes3WAfiles/";
     
     //this.server = "http://127.0.0.1:8000/api/";
     this.server = "http://phpoutils.eu-west-2.elasticbeanstalk.com/api/";
