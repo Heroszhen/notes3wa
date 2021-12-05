ff0c699d3562d89e9ccc7e6cfdbe8c467789a24c
zhen <zhen.yang@hotmail.fr>
2021-12-05 17:54:53 +0100

    maj

--- src/app/myservices/bases.service.ts
+++ src/app/myservices/bases.service.ts
@@ -9,10 +9,10 @@ export class BasesService {
   protected server:string;
   constructor() {
     //this.filesUrl = "http://localhost:8000/";
-    this.filesUrl = "https://notes3wa.yangzhen.fr/notes3WAfiles/";
+    this.filesUrl = "http://notes3wa.yangzhen.fr/notes3WAfiles/";
     
     //this.server = "http://127.0.0.1:8000/api/";
-    this.server = "https://notesaws.yangzhen.fr/api/";
+    this.server = "https://www.yangzhen.fr/api/";
   }
 
   getFileUrl(){
