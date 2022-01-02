6b92850013712ff47a618f5f75d91213e4715d61
zhen <zhen.yang@hotmail.fr>
2022-01-02 11:48:06 +0100

    maj

--- src/app/mycomponents/portfolio/portfolio.component.ts
+++ src/app/mycomponents/portfolio/portfolio.component.ts
@@ -12,10 +12,13 @@ export class PortfolioComponent implements OnInit {
   title2 = "Développeur web";
   index = 0;
   whitenav = false;
-  photos = ["html.png" , "css.png","js.png","php.png","bootstrap_logo.png","jquery.png","mysql.png","angular.png","symfony.png","express.png","mongodb.png","github.png"];
+  photos = ["html.png" , "css.png","js.png","php.png","bootstrap_logo.png","jquery.png","mysql.png","angular.png","symfony.png","express.png","mongodb.png","github.png","sveltejs.png"];
   folio = [
-    {"photo":"si_centraline.png","title":"si.mycentraline.fr","text":"L'application a été créée en Angular , c'est le système informatique en web de Centraline.."},
-    {"photo":"cloud_mobile.png","title":"Cloud de Centraline","text":"Le Cloud de Centraline fait en Ionic."},
+    {"photo":"wwwcooli.png","title":"www.cooli.fr","text":""},
+    {"photo":"erpcooli.png","title":"erp.cooli.fr","text":"C'est une application web ERP.(Angular + Symfony 4 + S3_AWS)"},
+    {"photo":"zdrive.png","title":"zdrive.yangzhen.fr","text":"C'est mon site de stockage et de partage de fichiers.(Angular + Symfony 4 + S3_AWS)"},
+    {"photo":"si_centraline.png","title":"si.mycentraline.fr","text":"L'application a été créée en Angular + Symfony 4, c'est le système informatique en web de Centraline.."},
+    {"photo":"cloud_mobile.png","title":"Cloud de Centraline","text":"Le Cloud de Centraline fait en Ionic + PHP."},
     {"photo":"canaljob.png","title":"Canaljob","text":"C'est un site de recherche d'emploi et de formation que j'ai refait en Symfony 4 pendant mon stage chez Canaljob."}
   ];
   bigimg:number = 0;
